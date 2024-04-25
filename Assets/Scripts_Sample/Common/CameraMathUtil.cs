5402d2b577f8cc49d05116c88d9aeb48f42b5196
onovich <hjn1110@163.com>
2024-04-26 05:46:00 +0800

    <WIP> GetModelDepth

--- Assets/Scripts_Sample/Common/CameraMathUtil.cs
+++ Assets/Scripts_Sample/Common/CameraMathUtil.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public static class CameraMathUtil {
 
-    // TRS
     public static float GetModelDepth(Vector2 screenPoint, Vector2 screenSize, float nearClip, float fov) {
         float ndcX = (2.0f * (screenPoint.x / screenSize.x) - 1.0f);
-        float ndcY = 1.0f - 2.0f * (screenPoint.y / screenSize.y);
-        float depth = nearClip / Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+        float ndcY = (2.0f * (screenPoint.y / screenSize.y) - 1.0f);
+
+        float angleX = Mathf.Atan(Mathf.Tan(fov * Mathf.Deg2Rad / 2.0f) * ndcX);
+        float angleY = Mathf.Atan(Mathf.Tan(fov * Mathf.Deg2Rad / 2.0f) * ndcY);
+
+        float depth = nearClip / Mathf.Cos(Mathf.Max(Mathf.Abs(angleX), Mathf.Abs(angleY)));
         return depth;
     }
 
