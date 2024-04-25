2cb318a433e30b50c5e7796ca6a2c2c26d571c6c
onovich <hjn1110@163.com>
2024-04-25 21:19:37 +0800

    Initial commit

---
+++ Assets/Scripts_Sample/CameraModel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraModel {
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public float FieldOfView;
+    public float AspectRatio;
+    public float NearClip;
+    public float FarClip;
+
+    public CameraModel(Vector3 position, Quaternion rotation, float fov, float aspectRatio, float nearClip, float farClip) {
+        Position = position;
+        Rotation = rotation;
+        FieldOfView = fov;
+        AspectRatio = aspectRatio;
+        NearClip = nearClip;
+        FarClip = farClip;
+    }
+
+    public Matrix4x4 GetProjectionMatrix() {
+        return Matrix4x4.Perspective(FieldOfView, AspectRatio, NearClip, FarClip);
+    }
+
+    public Matrix4x4 GetViewMatrix() {
+        Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Rotation);
+        Vector3 inversePosition = -Position;
+        Matrix4x4 translationMatrix = Matrix4x4.Translate(inversePosition);
+        return rotationMatrix * translationMatrix; // 注意矩阵乘法的顺序，先旋转后平移
+    }
+}
---
+++ Assets/Scripts_Sample/Sample.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour {
+
+    [SerializeField] GameObject objectPrefab;
+    [SerializeField] GameObject panelPrefab;
+    [SerializeField] Canvas canvas;
+    [SerializeField] int groundLayer;
+    [SerializeField] Camera mainCamera;
+
+    CameraModel cameraModel;
+    List<GameObject> objects;
+    List<GameObject> panels;
+
+    void Awake() {
+        objects = new List<GameObject>();
+        panels = new List<GameObject>();
+        cameraModel = new CameraModel(
+            position: mainCamera.transform.position,
+            rotation: mainCamera.transform.rotation,
+            fov: mainCamera.fieldOfView,
+            aspectRatio: mainCamera.aspect,
+            nearClip: mainCamera.nearClipPlane,
+            farClip: mainCamera.farClipPlane
+        );
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << groundLayer)) {
+                var depth = hit.distance;
+                Vector3 worldPos = CameraMathUtil.ScreenToWorldPos(cameraModel, Input.mousePosition, depth);
+                var _worldPos = hit.point;
+                GameObject obj = Instantiate(objectPrefab, worldPos, Quaternion.identity);
+                GameObject panel = Instantiate(panelPrefab, Vector2.zero, Quaternion.identity, canvas.transform);
+                objects.Add(obj);
+                panels.Add(panel);
+            }
+        }
+
+        for (int i = 0; i < panels.Count; i++) {
+            var panel = panels[i];
+            var worldPos = objects[i].transform.position;
+            Vector2 screenPos = CameraMathUtil.WorldToScreenPos(cameraModel, worldPos);
+            var _screenPos = mainCamera.WorldToScreenPoint(worldPos);
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.GetComponent<RectTransform>(), screenPos, null, out var canvasPos);
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.GetComponent<RectTransform>(), _screenPos, null, out var _canvasPos);
+
+            panel.transform.localPosition = canvasPos;
+            // panel.transform.localPosition = _canvasPos;
+
+            // Debug.Log($"WorldPos: {worldPos}, ScreenPos: {screenPos} - _{_screenPos}, CanvasPos: {canvasPos} - {_canvasPos}");
+        }
+    }
+
+    void OnDestroy() {
+        foreach (var obj in objects) {
+            Destroy(obj);
+        }
+        foreach (var panel in panels) {
+            Destroy(panel);
+        }
+        objects.Clear();
+        panels.Clear();
+    }
+
+}
